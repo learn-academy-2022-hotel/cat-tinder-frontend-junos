b6a5868709ba1d68e5eeea37e0a5dc1884c077d4
chrisyhlee <leechr8393@gmail.com>
2023-02-04 10:31:33 -0800

    making PR to merge

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react"
-import mockToons from './mockToon.js'
 import { Routes, Route } from "react-router-dom"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
